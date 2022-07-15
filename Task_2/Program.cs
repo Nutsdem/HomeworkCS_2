baac20b8fc6d17459a6314815270c7fbd70e27fc
nutsdem <nechaeff91@gmail.com>
2022-07-15 21:22:59 +0500

    Task_3

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число = ");
+int num = int.Parse(Console.ReadLine());
+
+if(num < 100)
+{
+    Console.WriteLine("Третего числа нет!");
+}
+if(num > 99 & num < 1000)
+{
+    Console.WriteLine((num - (num % 100)) / 100);
+}
+
+if (num > 999)
+{
+    Console.WriteLine(((num % 1000) - (num % 100)) % 10);
+}
+else
+{
+    Console.WriteLine(((num % 1000) - (num % 100)) % 10);
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число = ");
+int num = int.Parse(Console.ReadLine());
+int day = 0;
+if(num < 1 ^ num > 7)
+{
+    Console.WriteLine("Нет такого дня недели.");
+}
+else
+{
+    day = num;
+}
+if(day == 6 ^ day == 7)
+{
+    Console.WriteLine("Выходной");
+}
+
+if(day == 1 ^ day == 2 ^ day == 3 ^ day == 4 ^ day == 5)
+{
+    Console.WriteLine("На завод");
+}
